fc7c688553691806968bf5e3cb101c6f9af0d56f
Michael Nguy <michaelnguy@Michaels-MacBook-Pro.local>
2024-08-13 13:07:59 -0700

    Commit for heroku

--- app.py
+++ app.py
@@ -202,5 +202,5 @@ def predict():
         return jsonify({"error": str(e)}), 400
 
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
